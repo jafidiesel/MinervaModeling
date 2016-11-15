cae7465cb45458a4395aaa1e14fa75bc83badf1b
Emily <emilangui@gmail.com>
2016-11-14 22:52:18 -0300

    clave primaria en xpand

---
+++ ModeloERXpand.generator.project/bin/modelo/generic.sql
@@ -0,0 +1,27 @@
+
+	
+CREATE TABLE Entidad2-Debil/EntidadDebil{
+
+
+nombreAtributoEntidad1-Fuerte Float(255),
+
+
+
+}
+
+	
+CREATE TABLE Entidad1-Fuerte/EntidadFuerte{
+
+
+DNI varchar(255) NOT NULL ,
+
+
+
+Nombre varchar(255) NOT NULL ,
+
+
+
+PRIMARY KEY (DNI)
+
+}
+
