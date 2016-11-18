4ff82cb720e48cfb7084261bf3bd060ebed05ddb
jafidiesel <jafibravin@outlook.com>
2016-11-18 02:56:38 -0300

    Rformule el codigo para ingresr desde otra form que permite acceder a mas informacion. Resulta que antes solo accediamos por entidadFuerteDelDiagrama y entidadDebilDelDiagrama. EL problema era cuando queriamos ver cual entidad (debil o fuert) estaba relacionada con cual.
    Ahora el codigo ingresa por entidadesRelcionesFuertesDelDiagrama y entidadesRelcionesDebilDelDiagrama lo que nos permite acceder a mas elementos.
    
    Agrego imagenes ilustrativas
    
    PD: Aun no agrega la PK de un entidad fuerte en una entidad debil como FK
    
    Falta:
    - Clave primaria de Entidad fuerte en EntidadDebil como FK
    - Herencia
    - Clave primaria foranea
    - Relacion N:N
    - PK(EntidadFuerte) -> FK(EntidadDebil)

--- ModeloERXpand.generator.project/src-gen/modelo/generic.sql
+++ ModeloERXpand.generator.project/src-gen/modelo/generic.sql
@@ -1,7 +1,42 @@
 
 	
-CREATE TABLE Entidad2-Debil/EntidadDebil{
+entro a entidadRelacion
+	
+	CREATE TABLE Entidad1-Fuerte{
+	
+nombrePK varchar(255) NOT NULL ,
 
+nombreAtributoMultivaluado varchar(255) NOT NULL ,
+
+nombreFK varchar(255) NOT NULL ,
+
+	
+PRIMARY KEY (nombrePK)
+
+	
+FOREIGN KEY (nombreFK)
+
+	
+FOREIGN KEY (nombreAtributoMultivaluado)(Mutltivaluado-test)
+
+	}
+	
+	
+CREATE TABLE nombreAtributoMultivaluado+"/EntidadMultivaluada"»{
+OIDMultivaluado VARCHAR (255),
+nombreAtributoMultivaluado varchar(255) NOT NULL ,
+PRIMARY KEY (OIDMultivaluado,nombreAtributoMultivaluado)
+}
+
+	
+
+
+
+	
+entro a entidadRelacion
+	
+	CREATE TABLE Entidad2-Debil{
+	
 nombreAtributoEntidad1-Fuerte Float(255),
 
 7nombreClavePrimaria varchar(255) NOT NULL ,
@@ -10,53 +45,25 @@ nombreAtributoEntidad1-Fuerte Float(255),
 
 7nombreClaveForanea varchar(255) NOT NULL ,
 
-
+	
 PRIMARY KEY (7nombreClavePrimaria)
 
-
+	
 FOREIGN KEY (7nombreClaveForanea)
 
-
+	
 FOREIGN KEY (7nombreAtributoMultivaluado)(Mutltivaluado-test)
 
-}
-
+	}
 	
-CREATE TABLE Entidad1-Fuerte/EntidadFuerte{
-
-nombrePK varchar(255) NOT NULL ,
-
-nombreAtributoMultivaluado varchar(255) NOT NULL ,
-
-nombreFK varchar(255) NOT NULL ,
-
-
-PRIMARY KEY (nombrePK)
-
-
-FOREIGN KEY (nombreFK)
-
-
-FOREIGN KEY (nombreAtributoMultivaluado)(Mutltivaluado-test)
-
-}
-
 	
-
-CREATE TABLE 7nombreAtributoMultivaluado+"/EntidadFuerteMultivaluado"Â»{
+CREATE TABLE 7nombreAtributoMultivaluado+"/EntidadMultivaluada"»{
 OIDMultivaluado VARCHAR (255),
 7nombreAtributoMultivaluado varchar(255) NOT NULL ,
 PRIMARY KEY (OIDMultivaluado,7nombreAtributoMultivaluado)
 }
 
-
 	
 
-
-CREATE TABLE nombreAtributoMultivaluado+"/EntidadFuerteMultivaluado"Â»{
-OIDMultivaluado VARCHAR (255),
-nombreAtributoMultivaluado varchar(255) NOT NULL ,
-PRIMARY KEY (OIDMultivaluado,nombreAtributoMultivaluado)
-}
-
+	
 
