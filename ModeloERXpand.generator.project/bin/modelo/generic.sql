d23bae2fd9038c1096e6533e4febdf65d86c52db
Alan <kiriost.s@gmail.com>
2016-11-19 00:23:39 -0300

    Formateo de código de plantilla de Xpand

--- ModeloERXpand.generator.project/bin/modelo/generic.sql
+++ ModeloERXpand.generator.project/bin/modelo/generic.sql
@@ -1,31 +1,31 @@
 
 	
-entro a entidadRelacion
+	/* entro a entidadRelacion */
 	
-	CREATE TABLE Entidad1-Fuerte{
+CREATE TABLE Entidad1-Fuerte {
 	
-nombrePK varchar(255) NOT NULL ,
+	nombrePK varchar(255) NOT NULL ,
 
-nombreAtributoMultivaluado varchar(255) NOT NULL ,
+	nombreAtributoMultivaluado varchar(255) NOT NULL ,
 
-nombreFK varchar(255) NOT NULL ,
-
-	
-PRIMARY KEY (nombrePK)
+	nombreFK varchar(255) NOT NULL ,
 
 	
-FOREIGN KEY (nombreFK)
+	PRIMARY KEY (nombrePK)
 
 	
-FOREIGN KEY (nombreAtributoMultivaluado)(Mutltivaluado-test)
+	FOREIGN KEY (nombreFK)
 
-	}
+	
+	FOREIGN KEY (nombreAtributoMultivaluado) /* (Mutltivaluado-test) */
+
+}
 	
 	
-CREATE TABLE nombreAtributoMultivaluado+"/EntidadMultivaluada"»{
-OIDMultivaluado VARCHAR (255),
-nombreAtributoMultivaluado varchar(255) NOT NULL ,
-PRIMARY KEY (OIDMultivaluado,nombreAtributoMultivaluado)
+CREATE TABLE nombreAtributoMultivaluado+"/EntidadMultivaluada"Â»{
+	OIDMultivaluado VARCHAR (255),
+	nombreAtributoMultivaluado varchar(255) NOT NULL ,
+	PRIMARY KEY (OIDMultivaluado,nombreAtributoMultivaluado)
 }
 
 	
@@ -33,34 +33,34 @@ PRIMARY KEY (OIDMultivaluado,nombreAtributoMultivaluado)
 
 
 	
-entro a entidadRelacion
+	/* entro a entidadRelacion */
 	
-	CREATE TABLE Entidad2-Debil{
+CREATE TABLE Entidad2-Debil {
 	
-nombreAtributoEntidad1-Fuerte Float(255),
+	nombreAtributoEntidad1-Fuerte Float,
 
-7nombreClavePrimaria varchar(255) NOT NULL ,
+	7nombreClavePrimaria varchar(255) NOT NULL ,
 
-7nombreAtributoMultivaluado varchar(255) NOT NULL ,
+	7nombreAtributoMultivaluado varchar(255) NOT NULL ,
 
-7nombreClaveForanea varchar(255) NOT NULL ,
-
-	
-PRIMARY KEY (7nombreClavePrimaria)
+	7nombreClaveForanea varchar(255) NOT NULL ,
 
 	
-FOREIGN KEY (7nombreClaveForanea)
+	PRIMARY KEY (7nombreClavePrimaria)
 
 	
-FOREIGN KEY (7nombreAtributoMultivaluado)(Mutltivaluado-test)
+	FOREIGN KEY (7nombreClaveForanea)
 
-	}
+	
+	FOREIGN KEY (7nombreAtributoMultivaluado) /* (Mutltivaluado-test) */
+
+}
 	
 	
-CREATE TABLE 7nombreAtributoMultivaluado+"/EntidadMultivaluada"»{
-OIDMultivaluado VARCHAR (255),
-7nombreAtributoMultivaluado varchar(255) NOT NULL ,
-PRIMARY KEY (OIDMultivaluado,7nombreAtributoMultivaluado)
+CREATE TABLE 7nombreAtributoMultivaluado+"/EntidadMultivaluada"Â»{
+	OIDMultivaluado VARCHAR (255),
+	7nombreAtributoMultivaluado varchar(255) NOT NULL ,
+	PRIMARY KEY (OIDMultivaluado,7nombreAtributoMultivaluado)
 }
 
 	
